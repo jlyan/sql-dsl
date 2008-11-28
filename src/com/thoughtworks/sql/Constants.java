255a4fa4e7e933c0f14f5703b02b5ae547ddf91a
skyairmj <skyairmj@425b5e3a-bc29-11dd-9cb5-179210ed59e1>
2008-11-28 15:27:46 +0000

    [ming] - refactor the Criterion and Operator module - add TODO

--- src/com/thoughtworks/sql/Constants.java
+++ src/com/thoughtworks/sql/Constants.java
@@ -9,12 +9,6 @@ public class Constants {
     static final String ON = "ON";
     static final String JOIN = "JOIN";
     static final String ALL = "*";
-    static final String EQUAL_OPERATOR = "=";
-    static final String NOT_EQUAL_OPERATOR = "<>";
-    static final String GREATER_OPERATOR = ">";
-    static final String LESS_OPERATOR = "<";
-    static final String IS_NULL = "IS NULL";
-    static final String IS_NOT_NULL = "IS NOT NULL";
     static final String LEFT_PARENTHESIS = "(";
     static final String RIGHT_PARENTHESIS = ")";
     static final String AND = "AND";
@@ -24,5 +18,5 @@ public class Constants {
     static final String ORDER_BY = "ORDER BY";
     static final String GROUP_BY = "GROUP BY";
     static final String WHERE = "WHERE";
-    public static final String IN = "IN";
+    public static final String EXISTS = "EXISTS";
 }
